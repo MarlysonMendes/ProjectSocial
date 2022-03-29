e4cd91fd7aa42409771744afb15a7614bf72ee9c
Marlyson Mendes <marlysonmendes@gmail.com>
2022-03-29 02:54:10 -0300

    ValidateGuid

---
+++ CwkSocial.Api/Filters/ValidateGuidAttribute.cs
@@ -0,0 +1,30 @@
+﻿using CwkSocial.Api.Contracts.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CwkSocial.Api.Filters
+{
+    public class ValidateGuidAttribute : ActionFilterAttribute
+    {
+        private readonly string _key;
+        public ValidateGuidAttribute(string key)
+        {
+            _key = key;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ActionArguments.TryGetValue(_key, out var value)) return;
+            if (Guid.TryParse(value?.ToString(), out var guid)) return;
+
+            var apiError = new ErrorResponse();
+            apiError.StatusCode = 400;
+            apiError.StatusPhrase = "Bad request";
+            apiError.Timestamp = DateTime.Now;
+            context.Result = new ObjectResult(apiError);
+
+            apiError.Errors.Add($"the identifier for {_key} is not a correct Guid format");
+            context.Result = new ObjectResult(apiError);
+        }
+    }
+}
