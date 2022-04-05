a79acef645b558b550d80a63f892ba9d6c06048e
Marlyson Mendes <marlysonmendes@gmail.com>
2022-04-05 01:19:12 -0300

    add AuthenticationScheme

---
+++ CwkSocial.Api/Registrars/IdentityRegistrar.cs
@@ -0,0 +1,45 @@
+﻿using CwkSocial.Application.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace CwkSocial.Api.Registrars
+{
+    public class IdentityRegistrar : IWebApplicationBuilderRegistrar
+    {
+        public void RegisterServices(WebApplicationBuilder builder)
+        {
+            var jwtSettings = new JwtSettings();
+            builder.Configuration.Bind(nameof(JwtSettings),jwtSettings);
+
+            var jwtSection = builder.Configuration.GetSection(nameof(JwtSettings));
+            builder.Services.Configure<JwtSettings>(jwtSection);
+
+            builder.Services.AddAuthentication(e =>
+            {
+                e.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                e.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                e.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            
+            })
+             .AddJwtBearer(jwt =>
+             {
+                 jwt.SaveToken = true;
+                 jwt.TokenValidationParameters = new TokenValidationParameters
+                 {
+                     ValidateIssuerSigningKey = true,
+                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.SingningKey)),
+                     ValidateIssuer = true,
+                     ValidIssuer = jwtSettings.Issuer,
+                     ValidateAudience = true,
+                     ValidAudiences = jwtSettings.Audiences,
+                     RequireExpirationTime = false,
+                     ValidateLifetime = true
+                 };
+                 jwt.Audience = jwtSettings.Audiences[0];
+                 jwt.ClaimsIssuer = jwtSettings.Issuer;
+             });
+
+        }
+    }
+}
