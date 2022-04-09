4400ed967f321cb13e197e5c1abcca1d82175e80
Marlyson Mendes <marlysonmendes@gmail.com>
2022-04-09 09:04:24 -0300

    criando identity

--- CwkSocial.Application/Enums/ErrorCodes.cs
+++ CwkSocial.Application/Enums/ErrorCodes.cs
@@ -4,7 +4,13 @@
     {
         NotFound = 404,
         ServerError = 500,
+
+        // Validation errors should be in the range 100 - 199
         ValidationError = 101,
+        //Infrastruture erros should be in range 200-299
+        IdentityUserAlreadyExists = 201,
+        IdentityUserCreateFailed = 202,
+
         UnknownError = 999
 
     }
--- CwkSocial.Application/Identity/Handlers/RegisterIdentityCommandHandler.cs
+++ CwkSocial.Application/Identity/Handlers/RegisterIdentityCommandHandler.cs
@@ -1,6 +1,9 @@
-﻿using CwkSocial.Application.Identity.Commands;
+﻿using CwkSocial.Application.Enums;
+using CwkSocial.Application.Identity.Commands;
 using CwkSocial.Application.Models;
 using CwkSocial.Dal;
+using CwkSocial.Domain.Aggregates.UserProfileAggregate;
+using CwkSocial.Domain.Exceptions;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 using System;
@@ -22,9 +25,77 @@ namespace CwkSocial.Application.Identity.Handlers
             _ctx = ctx;
             _userManager = userManager;
         }
-        public Task<OperationResult<string>> Handle(RegisterIdentityCommand request, CancellationToken cancellationToken)
+
+        public async Task<OperationResult<string>> Handle(RegisterIdentityCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var result = new OperationResult<string>();
+            try
+            {
+                var existingIdentity = _userManager.FindByEmailAsync(request.UserName);
+
+                if (existingIdentity != null)
+                {
+                    result.IsError = true;
+                    var error = new Error { Code = ErrorCode.IdentityUserAlreadyExists, 
+                        Message = "Provided email address already exists. Cannot register ne user" };
+                    result.Errors.Add(error);
+                    return result;
+                }
+                var identity = new IdentityUser
+                {
+                    Email = request.UserName,
+                    UserName = request.UserName
+                };
+               
+                using var transaction = _ctx.Database.BeginTransaction();
+                var createIdentity = await _userManager.CreateAsync(identity, request.Password);
+                
+                if (!createIdentity.Succeeded)
+                {
+                    result.IsError = true;
+
+                    foreach (var identityError in createIdentity.Errors)
+                    {
+                        var error = new Error
+                        {
+                            Code = ErrorCode.IdentityUserCreateFailed,
+                            Message = identityError.Description
+                        };
+                        result.Errors.Add(error);
+                    }                    
+                    return result;
+                }
+                var basicInfo = BasicInfo.CreateBasicInfo(request.FirstName, request.LastName, request.UserName,
+                        request.Phone, request.DateOfBirth, request.CurrentCity);
+
+                var profile = UserProfile.CreateUserProfile(identity.Id, basicInfo);
+
+                _ctx.UserProfiles.Add(profile);
+                await _ctx.SaveChangesAsync();
+                await transaction.CommitAsync();
+            }
+            catch (UserProfileNotValidException ex)
+            {
+                result.IsError = true;
+                ex.ValidationErrors.ForEach(e =>
+                {
+                    var error = new Error
+                    {
+                        Code = ErrorCode.ValidationError,
+                        Message = $"{ex.Message}"
+                    };
+                    result.Errors.Add(error);
+                });
+            }
+            catch (Exception ex)
+            {
+                var error = new Error { Code = ErrorCode.UnknownError, 
+                    Message = ex.Message };
+                result.IsError = true;
+                result.Errors.Add(error);
+            }
+
+            return result;
         }
     }
 }
